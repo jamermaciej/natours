00942a3e345e993bb6208e28ee2d52d86af7969a
mjamer <maciej.jamer@af.agency>
2023-11-20 17:58:26 +0100

    add footer

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -2,17 +2,21 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './ui/header/header.component';
+import { FooterComponent } from './ui/footer/footer.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
   template: `
     <!-- HEADER -->
     <app-header></app-header>
 
     <!-- CONTENT-->
     <router-outlet></router-outlet>
+
+    <!-- FOOTER-->
+    <app-footer></app-footer>
   `
 })
 export class LayoutComponent {
