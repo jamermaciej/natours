34f6908e69671fbec05b7b7b205d6bf827a2d1c4
mjamer <maciej.jamer@af.agency>
2024-03-06 14:40:39 +0100

    remove environments fro mgit

--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    apiUrl: 'https://natours-app-1cf734474c20.herokuapp.com/api/v1',
-    apiHostUrl: 'https://natours-app-1cf734474c20.herokuapp.com/'
-};
