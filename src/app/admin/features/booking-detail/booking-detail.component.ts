d6284d2494ce96615a34935a06456bdd96bbaec2
Maciej Jamer <maciek77jamer@gmail.com>
2026-03-31 01:54:50 +0200

    update cancellation reason select

--- src/app/admin/features/booking-detail/booking-detail.component.ts
+++ src/app/admin/features/booking-detail/booking-detail.component.ts
@@ -105,6 +105,7 @@ export class BookingDetailComponent {
       data: booking,
       disableClose: false,
       hasBackdrop: true,
+      autoFocus: false,
       backdropClass: 'cdk-overlay-backdrop',
     });
 
--- src/app/admin/features/cancellation-modal/cancellation-modal.component.ts
+++ src/app/admin/features/cancellation-modal/cancellation-modal.component.ts
@@ -1,20 +1,28 @@
-import { Component, computed, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal, viewChild } from '@angular/core';
 import { ModalLayoutComponent } from '../../../shared/ui/modal-layout/modal-layout.component';
 import { DIALOG_DATA, DialogRef } from '@angular/cdk/dialog';
 import { HttpErrorResponse } from '@angular/common/http';
 import { BookingsStore } from '../../data-access/bookings-store';
 import { SnackbarService } from '../../../shared/services/snackbar.service';
 import { Booking } from '../../../bookings/interfaces/booking';
-import { MatOption, MatSelect } from '@angular/material/select';
+import { MatError, MatFormField, MatLabel, MatOption, MatSelect } from '@angular/material/select';
 import { CancellationReason } from '../../../bookings/models/cancellation-reason';
 import { EnumLabelPipe } from '../../../shared/pipes/enum-label.pipe';
-import { KeyValuePipe } from '@angular/common';
 import { CANCELLATION_REASON_LABELS } from '../../../bookings/models/cancellation-reason-labels';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-cancellation-modal',
-  imports: [ModalLayoutComponent, MatSelect, MatOption, EnumLabelPipe, KeyValuePipe, FormsModule],
+  imports: [
+    ModalLayoutComponent,
+    MatSelect,
+    MatOption,
+    EnumLabelPipe,
+    FormsModule,
+    MatFormField,
+    MatLabel,
+    MatError,
+  ],
   templateUrl: './cancellation-modal.component.html',
   styleUrl: './cancellation-modal.component.scss',
 })
@@ -24,6 +32,8 @@ export class CancellationModalComponent {
   protected dialogRef = inject(DialogRef<Booking>);
   protected readonly booking = inject<Booking>(DIALOG_DATA);
 
+  private readonly reasonSelectRef = viewChild<NgModel>('reasonSelect');
+
   readonly isLoading = signal(false);
   readonly error = signal<string | null>(null);
   protected reason = signal<CancellationReason | null>(null);
@@ -31,14 +41,12 @@ export class CancellationModalComponent {
   readonly isValid = computed(() => !!this.reason());
   protected showErrors = signal(false);
 
-  protected readonly reasonError = computed(() =>
-    this.showErrors() && !this.reason() ? 'Reason is required' : null,
-  );
-
-  protected readonly cancellationReason = CancellationReason;
+  protected readonly cancellationReason = Object.values(CancellationReason);
   protected readonly cancellationReasonLabel = CANCELLATION_REASON_LABELS;
 
   async onConfirm() {
+    this.reasonSelectRef()?.control.markAsTouched();
+
     if (!this.isValid()) {
       this.showErrors.set(true);
       return;
