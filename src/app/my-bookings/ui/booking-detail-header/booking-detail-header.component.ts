0cf15385b73fbdc188a44da746feb20a80abec44
Maciej Jamer <maciek77jamer@gmail.com>
2026-04-08 17:15:51 +0200

    add book again button

--- src/app/my-bookings/ui/booking-detail-header/booking-detail-header.component.ts
+++ src/app/my-bookings/ui/booking-detail-header/booking-detail-header.component.ts
@@ -1,10 +1,12 @@
 import { Component, input, output } from '@angular/core';
 import { BookingStatus } from '../../../tours/enums/booking-status';
 import { StatusBadgeComponent } from '../../../shared/ui/status-badge/status-badge.component';
+import { RouterLink } from '@angular/router';
+import { FlowRoutes } from '../../../shared/enums/flow-routes';
 
 @Component({
   selector: 'app-booking-detail-header',
-  imports: [StatusBadgeComponent],
+  imports: [StatusBadgeComponent, RouterLink],
   templateUrl: './booking-detail-header.component.html',
   styleUrl: './booking-detail-header.component.scss',
 })
@@ -13,7 +15,10 @@ export class BookingDetailHeaderComponent {
   readonly status = input.required<BookingStatus>();
   readonly paid = input.required<boolean>();
   readonly canCancel = input.required<boolean>();
+  readonly tourSlug = input.required<string>();
   protected readonly bookingCancelled = output<void>();
+  protected readonly bookingStatus = BookingStatus;
+  protected readonly flowRoutes = FlowRoutes;
 
   onCancelBooking() {
     this.bookingCancelled.emit();
